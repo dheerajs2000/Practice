246ed9c9fa0fce1e3a59870c09125819cbfcc96a
dheerajs2000 <159991382+dheerajs2000@users.noreply.github.com>
2024-07-28 11:09:22 +0530

    Add files via upload

---
+++ diagonalDifference.py
@@ -0,0 +1,8 @@
+def diagonalDifference(arr):
+    n=len(arr)
+    sum1,sum2=0,0
+    for i in range(n):
+        sum1+=arr[i][i]
+        sum2+=arr[i][n-1-i]
+    return abs(sum1-sum2)
+print(diagonalDifference([[11,2,3],[22,44,2],[33,6,1]]))
